140f650396583f701a060619bf740222d84f5395
FEvelychko <maxdevelopenko@gmail.com>
2015-10-08 08:27:25 +0300

    last updates

--- app/controller.js
+++ app/controller.js
@@ -6,10 +6,10 @@
     var booksControllers = angular.module('booksControllers', []);
 
     booksControllers.controller('BookListController', ['$scope', '$http', function($scope,$http){
-        $http.get('books/books.json').success(function(data){
+        $http.get('data/books/books.json').success(function(data){
             $scope.books = data;
         });
-        $http.get('genres/genres.json').success(function(data){
+        $http.get('data/genres/genres.json').success(function(data){
             $scope.genres = data;
         });
         //$scope.orderProp = 'age';
@@ -18,12 +18,12 @@
     }]);
     booksControllers.controller('BookDetailController', ['$scope', '$routeParams', '$http', function($scope,$routeParams,$http){
         //$scope.bookId = $routeParams.bookId;
-        $http.get('books/' + $routeParams.bookId + '.json').success(function(data){
+        $http.get('data/books/' + $routeParams.bookId + '.json').success(function(data){
             $scope.book = data;
         });
     }]);
     booksControllers.controller('WriterDetailController', ['$scope', '$routeParams', '$http', function($scope,$routeParams,$http){
-        $http.get('writers/' + $routeParams.writerId + '.json').success(function(data){
+        $http.get('data/writers/' + $routeParams.writerId + '.json').success(function(data){
             $scope.writer = data;
         })
     }]);
